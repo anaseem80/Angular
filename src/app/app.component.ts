02060b979fd6665364934d832cb44f68e70f7e4f
anaseem80 <anaseemamin@gmail.com>
2024-06-05 23:54:18 +0300

    Data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,27 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
+  standalone: true, // Smoothly run the component
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angluar';
+  title: string = 'Angular';
+
+  // atr binding
+  isDisabled: boolean = false;
+  theImage: string = '/assets/1.jpg'
+
+  // style binding
+  bgColor: string = 'green';
+  titleColor: string = 'white'
+  description: string = 'font-size: 50px; color: #e50'
+
+  onButtonClick() {
+    this.isDisabled = !this.isDisabled
+  }
+
+  // class binding
+  redText: string = 'abcd'
 }
